3c0ee767d93679b288b94abf530be687b14f4e03
NganBuiNguyen <nganbui180995@gmail.com>
2017-02-08 18:32:35 +0700

    NganBui

---
+++ src/app/dataConverter/Card.h
@@ -0,0 +1,28 @@
+#ifndef __Card_H_
+#define __Card_H_
+
+
+#include <iostream>
+#include <string>
+#include <string.h>
+
+
+  
+/*class Card 
+{
+public:
+    Card(); 
+    Card(std::string CardID);
+    Card(std::string CardID,std::string kindCard);
+    std::string getCardID();
+    void setCardID(std::string cardID);
+    std::string getKindCard();
+    void setKindCard(std::string kindCard);
+private:
+    std::string cardID;
+    std::string kindCard;
+
+};*/
+
+
+#endif
