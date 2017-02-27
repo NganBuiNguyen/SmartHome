d618ef2ad86103f82c21dc3810fe72023f7641ce
NganBuiNguyen <nganbui180995@gmail.com>
2017-02-27 07:46:15 +0700

    NganBui: create CFFI DBCard

---
+++ src/app/processor/cffi_interfaces/__cffi_dbCard.py
@@ -0,0 +1,95 @@
+import cffi
+import os
+
+#LIDT declare
+LIDT_ROOT = os.getenv('LIDT_ROOT')
+LIDT_LIBS = os.path.sep.join((LIDT_ROOT, 'obj', 'libs'))
+
+DATA_TYPE_DEF_INCLUDES = os.path.sep.join((LIDT_ROOT, 'src', 'app', 'dataConverter'))
+
+DBCARD_FOR_C_INCLUDES = os.path.sep.join((LIDT_ROOT, 'src', 
+                            'app', 'dbManager', 'cWrapperData'))
+DBCARD_FOR_C_LIBS = ["DbCardForC"]
+
+#define common libraries
+POCO_ROOT = os.getenv('POCO_ROOT')
+POCO_INCLUDES = os.path.sep.join((POCO_ROOT, 'include'))
+POCO_LIB_DIR = os.path.sep.join((POCO_ROOT, 'lib'))
+POCO_LIBS = ["PocoFoundation", 'PocoFoundationd', "PocoData", 'PocoDatad', 'stdc++']
+
+
+#define mysql libraries
+MYSQL_ROOT = os.getenv('MYSQL_ROOT')
+MYSQL_CONN_INCLUDES = os.path.sep.join((MYSQL_ROOT, 'include'))
+MYSQL_CONN_LIB_DIR = os.path.sep.join((MYSQL_ROOT, 'lib'))
+MYSQL_CONN_LIBS = ["mysqlcppconn", 'stdc++']
+
+dbCard_cffi = cffi.FFI()
+dbCard_cffi.cdef("""
+    #define NAME_CARD_PERSON_LENGTH 20
+    #define NAME_DOOR_ROOM 10
+    typedef struct
+    {
+        int year ;
+        int mon ;
+        int day ;
+        int sec ;   
+        int min ;   
+        int hour ;
+    }Date_Time;
+
+    typedef struct
+    {
+        int idPerson;
+        char namePerson[NAME_CARD_PERSON_LENGTH];
+        int age;
+        bool grantPerson;
+        int idRoom;
+    }Person;
+
+    typedef struct
+    {
+        char idCard[NAME_CARD_PERSON_LENGTH];
+        char nameKindCard[NAME_CARD_PERSON_LENGTH];
+        int idPerson;
+    }Card;
+
+    typedef struct
+    {
+        int idDoor;
+        char nameDoor[NAME_DOOR_ROOM]; 
+        char ip[20];
+        long port;
+    }Door;
+
+    typedef struct
+    {
+        int idRoom;
+        char nameRoom[NAME_DOOR_ROOM];
+        int idDoor;
+    }Room;
+
+    typedef struct
+      {
+        Date_Time dateTime;
+        Card card;
+        Person person;
+        Door door;
+        Room room;
+      }CardInfo;
+      
+    bool insert_to_db_Card_ForC(CardInfo* info);
+    bool select_to_db_Card_ForC(); 
+    bool update_to_db_Card_ForC(CardInfo* info);
+    bool delete_to_db_Card_ForC(CardInfo* info);
+""")
+
+dbCard_c = dbCard_cffi.verify("""
+                #include "DbCardForC.h"
+                #include "DataDef.h"
+                """,
+                include_dirs = [DBCARD_FOR_C_INCLUDES, POCO_INCLUDES, 
+                               MYSQL_CONN_INCLUDES, DATA_TYPE_DEF_INCLUDES],
+                libraries = DBCARD_FOR_C_LIBS + MYSQL_CONN_LIBS + POCO_LIBS,
+                library_dirs = [LIDT_LIBS, MYSQL_CONN_LIB_DIR, POCO_LIB_DIR],
+                modulename = "__cffi_dbCard")
