c9e319742393bd24fc25dff0274e14c352822c65
ngan-bui <nganbui180995@gmail.com>
2017-05-15 18:02:53 +0700

    NganBui: Select funtion.

---
+++ src/app/dbManager/cWrapperData/test/TestDbCardForC.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+#include "DbCardForC.h"
+
+
+// static sql::Driver* MYSQL_DRIVER_INSTANCE = get_driver_instance();
+// static sql::Connection* MYSQL_DB_CONNECTION =
+//                     MYSQL_DRIVER_INSTANCE->connect(DBHOST, USER, PASSWORD);
+
+class TestDbCardForC : public ::testing::Test
+{
+public:
+protected:
+    virtual void SetUp()
+    {
+    }
+
+    virtual void TearDown()
+    {
+    }
+};
+
+
+TEST_F(TestDbCardForC, TestSelectCardForC_RESULT_OK)
+{
+    CardInfo* info = NULL;
+    int numberOfElement = 0;
+    bool status = dbcardForC.selectToDbCardForC(&info, &numberOfElement);
+    
+    EXPECT_TRUE(status);
+    
+}
---
+++ src/app/processor/test/test_db_card.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestSelectCardMethods(unittest.TestCase):
+
+    def test_selectCard(self):
+        self.assertTrue(select_card_info())
+
+if __name__ == '__main__':
+    unittest.main()
